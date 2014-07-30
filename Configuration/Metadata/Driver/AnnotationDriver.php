b87cbfbfefe76c7ba8fa3f258c421db378f5f50f
Jérémy Jégou <jeremy.jegou@fr.airtag.com>
2014-07-30 11:21:18 +0200

    Remove unused functions, prepare annotation driver
    Signed-off-by: Jérémy Jégou <jeremy.jegou@fr.airtag.com>

--- Configuration/Metadata/Driver/AnnotationDriver.php
+++ Configuration/Metadata/Driver/AnnotationDriver.php
@@ -69,67 +69,8 @@ class AnnotationDriver implements DriverInterface
         $classMetadata->fileResources[] = $class->getFilename();
 
         foreach ($annotations as $annotation) {
-            if ($annotation instanceof Annotation\Relation) {
-                $classMetadata->addRelation(new Relation(
-                    $annotation->name,
-                    $this->createHref($annotation->href),
-                    $this->createEmbedded($annotation->embedded),
-                    $annotation->attributes ?: array(),
-                    $this->createExclusion($annotation->exclusion)
-                ));
-            } elseif ($annotation instanceof Annotation\RelationProvider) {
-                $classMetadata->addRelationProvider(new RelationProvider($annotation->name));
-            }
-        }
-
-        if (0 === count($classMetadata->getRelations()) && 0 === count($classMetadata->getRelationProviders())) {
-            return null;
         }
 
         return $classMetadata;
-    }
-
-    private function parseExclusion(Annotation\Exclusion $exclusion)
-    {
-        return new Exclusion(
-            $exclusion->groups,
-            $exclusion->sinceVersion,
-            $exclusion->untilVersion,
-            $exclusion->maxDepth,
-            $exclusion->excludeIf
-        );
-    }
-
-    private function createHref($href)
-    {
-        if ($href instanceof Annotation\Route) {
-            $href = new Route($href->name, $href->parameters, $href->absolute, $href->generator);
-        }
-
-        return $href;
-    }
-
-    private function createEmbedded($embedded)
-    {
-        if ($embedded instanceof Annotation\Embedded) {
-            $embeddedExclusion = $embedded->exclusion;
-
-            if (null !== $embeddedExclusion) {
-                $embeddedExclusion = $this->parseExclusion($embeddedExclusion);
-            }
-
-            $embedded = new Embedded($embedded->content, $embedded->xmlElementName, $embeddedExclusion);
-        }
-
-        return $embedded;
-    }
-
-    private function createExclusion($exclusion)
-    {
-        if (null !== $exclusion) {
-            $exclusion = $this->parseExclusion($exclusion);
-        }
-
-        return $exclusion;
     }
 }
